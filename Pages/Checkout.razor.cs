5bf0f321effa018b2c246b87d437fba29a840df1
mabyre <mabyre@sodevlog.com>
2023-02-22 10:51:08 +0100

    Ajoutez des fichiers projet.

---
+++ Pages/Checkout.razor.cs
@@ -0,0 +1,9 @@
+﻿using BlazingPizza.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingPizza.Pages;
+
+public class CheckoutBase : ComponentBase
+{
+    protected static Order order => OrderState.Order;
+}
